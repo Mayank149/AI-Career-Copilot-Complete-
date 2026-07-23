8d92f711619b8d7f5b53c9f06f88b307ac744b01
mayank bansal <bansalmayank1414@gmail.com>
2026-07-23 13:33:44 +0530

    Created chunks and embeddings object

--- routes/resume.py
+++ routes/resume.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, UploadFile, File
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 import os
 import shutil
 
@@ -13,7 +16,23 @@ async def upload_resume(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+    
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 100
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    
     return {
         "filename": file.filename,
-        "content_type": file.content_type
+        "file_path": file_path,
+        "num_pages": len(documents),
+        "num_chunks": len(chunks)
     }
