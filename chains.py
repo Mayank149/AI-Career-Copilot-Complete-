8dfbb58cd945a7b2e43bb5fb7d69cf0e9bcee26a
Mayank149 <bansalmayank1414@gmail.com>
2026-07-25 11:31:40 +0530

    seperated vector_store and retriever functions

--- chains.py
+++ chains.py
@@ -1,4 +1,11 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model = "gemma2:2b"
+)
 
 prompt = ChatPromptTemplate.from_template("""
 You are an AI Career Copilot.
@@ -14,5 +21,18 @@ Resume:
 Question:
 {question}
 """)
+
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+def create_rag_chain(retriever):
+    return (
+        {
+            "context" : retriever | format_docs,
+            "question" : RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
--- vectorstore.py
+++ vectorstore.py
@@ -5,13 +5,23 @@ embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-def create_retriever(chunks, persist_directory="./chroma_db"):
+def create_vector_store(chunks, persist_directory="./chroma_db"):
     vector_store = Chroma.from_documents(
         documents = chunks,
         embedding = embeddings,
         persist_directory = persist_directory
     )
+    return vector_store
 
+def load_vectore_store():
+    return Chroma(
+        persist_directory = "./chroma_db",
+        embedding_function = embeddings
+    )
+
+def create_retriever():
+    vector_store = load_vectore_store()
+    
     retriever = vector_store.as_retriever(
         search_type = "mmr",
         search_kwargs = {"k": 4},
